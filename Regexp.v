68812d92f914c401adc2795243049a9bb15b63f2
Thalia Archibald <thalia@archibald.dev>
2023-02-01 15:45:13 -0700

    Coerce ascii and string to regexp

--- Regexp.v
+++ Regexp.v
@@ -1,9 +1,10 @@
-Parameter A : Type.
+Require Import Ascii String.
+Open Scope string_scope.
 
 Inductive regexp :=
   | EmptySet
   | EmptyStr
-  | Lit (a : A)
+  | Lit (a : ascii)
   | Cat (re1 re2 : regexp)
   | Alt (re1 re2 : regexp)
   | Star (re : regexp).
@@ -12,6 +13,7 @@ Declare Custom Entry regexp.
 Declare Scope regexp_scope.
 Notation "// re //" := re (at level 0, re custom regexp at level 99) : regexp_scope.
 Notation "( re )" := re (in custom regexp, re at level 99) : regexp_scope.
+Notation "x" := x (in custom regexp at level 0, x constr at level 0) : regexp_scope.
 Notation "'∅'" := EmptySet (in custom regexp).
 Notation "'ε'" := EmptyStr (in custom regexp).
 Notation "re1 ; re2" := (Cat re1 re2) (in custom regexp at level 40, left associativity) : regexp_scope.
@@ -19,4 +21,14 @@ Notation "re1 | re2" := (Alt re1 re2) (in custom regexp at level 50, left associ
 Notation "re *" := (Star re) (in custom regexp at level 35, right associativity, format "re *") : regexp_scope.
 Open Scope regexp_scope.
 
-Coercion Lit : A >-> regexp.
+Local Notation "a :: s" := (String a s) : string_scope.
+
+Fixpoint regexp_of_string (s : string) : regexp :=
+  match s with
+  | a :: "" => Lit a
+  | a :: s' => Cat (Lit a) (regexp_of_string s')
+  | "" => EmptyStr
+  end.
+
+Coercion Lit : ascii >-> regexp.
+Coercion regexp_of_string : string >-> regexp.
