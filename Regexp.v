ce7b23fba3180692cae0e0a7fa26818d7d8a6de1
Thalia Archibald <thalia@archibald.dev>
2023-02-03 13:41:51 -0700

    Prove various match equivalences

--- Regexp.v
+++ Regexp.v
@@ -62,17 +62,84 @@ Inductive regexp_match : list ascii -> regexp -> Prop :=
 
   where "s =~ re" := (regexp_match s re).
 
+Lemma MCat_EmptySet_l : forall re s,
+  ~ (s =~ Cat EmptySet re).
+Proof.
+  unfold not. intros. inversion H; subst. inversion H3. Qed.
+
+Lemma MCat_EmptySet_r : forall re s,
+  ~ (s =~ Cat re EmptySet).
+Proof.
+  unfold not. intros. inversion H; subst. inversion H4. Qed.
+
+Lemma MCat_EmptyStr_l : forall re s,
+  s =~ Cat EmptyStr re <-> s =~ re.
+Proof.
+  split; intros.
+  - inversion H; subst. inversion H3; subst. assumption.
+  - rewrite <- (app_nil_l _). constructor. constructor. assumption. Qed.
+
+Lemma MCat_EmptyStr_r : forall re s,
+  s =~ Cat re EmptyStr <-> s =~ re.
+Proof.
+  split; intros.
+  - inversion H; inversion H4; subst. rewrite app_nil_r. assumption.
+  - rewrite <- (app_nil_r _). apply MCat. assumption. apply MEmpty. Qed.
+
+Lemma MAlt : forall s re1 re2,
+  s =~ Alt re1 re2 <-> s =~ re1 \/ s =~ re2.
+Proof.
+  split; intros.
+  - inversion H; subst; [left | right]; assumption.
+  - destruct H; [apply MAltL | apply MAltR]; assumption. Qed.
+
+Lemma MAlt_EmptySet_l : forall re s,
+  s =~ Alt EmptySet re <-> s =~ re.
+Proof.
+  split; intros.
+  - inversion H; subst. inversion H2. assumption.
+  - apply MAltR. assumption. Qed.
+
+Lemma MAlt_EmptySet_r : forall re s,
+  s =~ Alt re EmptySet <-> s =~ re.
+Proof.
+  split; intros.
+  - inversion H; subst. assumption. inversion H2.
+  - apply MAltL. assumption. Qed.
+
 Lemma MStar1 : forall s re,
   s =~ re -> s =~ Star re.
 Proof.
-  intros. rewrite <- (app_nil_r _).
-  repeat constructor. assumption. Qed.
+  intros. rewrite <- (app_nil_r _). apply MStarCat, MStar0. assumption. Qed.
 
-Lemma MAlt : forall s re1 re2,
-  s =~ re1 \/ s =~ re2 ->
-  s =~ Alt re1 re2.
+Lemma MStar_EmptySet : forall s,
+  s =~ Star EmptySet <-> s =~ EmptyStr.
 Proof.
-  intros. destruct H; constructor; assumption. Qed.
+  split; intros.
+  - inversion H; subst. apply MEmpty. inversion H1.
+  - inversion H; subst. apply MStar0. Qed.
+
+Lemma MClass0 : forall s,
+  s =~ Class [] <-> s =~ EmptySet.
+Proof.
+  split; intros; inversion H; subst. inversion H2. Qed.
+
+Lemma MClass1 : forall c s,
+  s =~ Class [c] <-> s =~ Lit c.
+Proof.
+  split; intros; inversion H; subst.
+  - inversion H2; subst. apply MLit. inversion H0.
+  - apply MClass, in_eq. Qed.
+
+Lemma MClassN : forall c cs s,
+  s =~ Class (c :: cs) <-> s =~ Alt (Lit c) (Class cs).
+Proof.
+  split; intros;
+  inversion H; inversion H2; subst.
+  - apply MAltL, MLit.
+  - apply MAltR, MClass. assumption.
+  - apply MClass, in_eq.
+  - apply MClass, in_cons. assumption. Qed.
 
 Fixpoint is_empty_set (re : regexp) : bool :=
   match re with
@@ -96,11 +163,6 @@ Fixpoint is_empty_str (re : regexp) : bool :=
   | Star re => is_empty_set re
   | Class _ => false
   end.
-
-Theorem empty_set_class : forall s,
-  s =~ EmptySet <-> s =~ Class [].
-Proof.
-  split; intros; inversion H; subst. destruct (in_nil H2). Qed.
 
 Theorem match_empty_set : forall re s,
   is_empty_set re = true -> ~ (s =~ re).
@@ -162,6 +224,31 @@ Fixpoint split_first (re : regexp) : option (list ascii * regexp) :=
   | Star re => None
   | Class cs => Some (cs, EmptyStr)
   end.
+
+Fixpoint split_last (re : regexp) : option (list ascii * regexp) :=
+  match re with
+  | EmptySet => None
+  | EmptyStr => None
+  | Lit c => Some ([c], EmptyStr)
+  | Cat re1 re2 =>
+      if is_empty_str re2 then split_last re1 else
+      match split_last re2 with
+      | Some (cs, re2') => Some (cs, Cat re1 re2')
+      | _ => None
+      end
+  | Alt re1 re2 =>
+      match split_last re1, split_last re2 with
+      | Some (cs1, re1'), Some (cs2, re2') => Some (cs1 ++ cs2, Alt re1' re2')
+      | _, _ => None
+      end
+  | Star re => None
+  | Class cs => Some (cs, EmptyStr)
+  end.
+
+Lemma split_first_cat : forall re cs re' s,
+  split_first re = Some (cs, re') ->
+  s =~ re <-> s =~ Cat (Class cs) re'.
+Proof. Admitted.
 
 Fixpoint combine_classes (re : regexp) : regexp.
 Admitted.
