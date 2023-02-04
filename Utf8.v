77932c00c4d035f73ba1e57ef3099c31fd44c8a9
Thalia Archibald <thalia@archibald.dev>
2023-02-03 19:56:18 -0700

    Validate UTF-8 while decoding

--- Utf8.v
+++ Utf8.v
@@ -1,3 +1,4 @@
+Require Import Bool.
 Require Import List. Import ListNotations.
 Require Import Ascii String.
 Require Import NArith.
@@ -11,20 +12,33 @@ Local Notation "a ::: s" := (String a s) (at level 60, right associativity) : st
 
 Fixpoint decode_utf8 (s : string) : list rune * bool :=
   match s with
-  | (0,b6,b5,b4,b3,b2,b1,b0) ::: s' => let (rs, ok) := decode_utf8 s' in
-      (N_of_digits [b0;b1;b2;b3;b4;b5;b6] :: rs, ok)
+  | (0,b6,b5,b4,b3,b2,b1,b0) ::: s' =>
+      let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6] in
+      let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
   | (1,1,0,b10,b9,b8,b7,b6) :::
-    (1,0,b5,b4,b3,b2,b1,b0) ::: s' => let (rs, ok) := decode_utf8 s' in
-      (N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10] :: rs, ok)
+    (1,0,b5,b4,b3,b2,b1,b0) ::: s' =>
+      let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10] in
+      if (r <? 128)%N then ([], false) else
+      let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
   | (1,1,1,0,b15,b14,b13,b12) :::
     (1,0,b11,b10,b9,b8,b7,b6) :::
-    (1,0,b5,b4,b3,b2,b1,b0) ::: s' => let (rs, ok) := decode_utf8 s' in
-      (N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10;b11;b12;b13;b14;b15] :: rs, ok)
+    (1,0,b5,b4,b3,b2,b1,b0) ::: s' =>
+      let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10;b11;b12;b13;b14;b15] in
+      if ((r <? 2048) || ((55296 <=? r) && (r <=? 57343)))%N then ([], false) else
+      let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
   | (1,1,1,1,0,b20,b19,b18) :::
     (1,0,b17,b16,b15,b14,b13,b12) :::
     (1,0,b11,b10,b9,b8,b7,b6) :::
-    (1,0,b5,b4,b3,b2,b1,b0) ::: s' => let (rs, ok) := decode_utf8 s' in
-      (N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10;b11;b12;b13;b14;b15;b16;b17;b18;b19;b20] :: rs, ok)
+    (1,0,b5,b4,b3,b2,b1,b0) ::: s' =>
+      let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10;b11;b12;b13;b14;b15;b16;b17;b18;b19;b20] in
+      if ((r <? 65536) || (1114111 <? r))%N then ([], false) else
+      let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
   | _ ::: _ => ([], false)
   | "" => ([], true)
+  end%string.
+
+Fixpoint string_is_ascii (s : string) : bool :=
+  match s with
+  | a ::: s' => (a <? "128")%char && string_is_ascii s'
+  | "" => false
   end%string.
