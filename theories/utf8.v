773877c1764ad5c80fa207b02c4edd542dc7d991
Thalia Archibald <thalia@archibald.dev>
2023-02-08 02:39:40 -0700

    Match on first byte before length in decode_utf8

--- theories/utf8.v
+++ theories/utf8.v
@@ -1,7 +1,6 @@
 Require Import Bool.
-Require Import List. Import ListNotations.
-Require Import Ascii String.
 Require Import NArith.
+From recross Require Import util.
 
 Definition rune := N.
 
@@ -29,30 +28,42 @@ Definition rune_of_bits (bs : rune_bits) : rune :=
 
 Fixpoint decode_utf8 (s : string) : list rune * bool :=
   match s with
-  | b[0,b6,b5,b4,b3,b2,b1,b0] ::: s' =>
+  | b[b7,b6,b5,b4,b3,b2,b1,b0] ::: s' =>
+    match b7, b6, b5, b4, b3 with
+    | 0, _, _, _, _ =>
       let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6] in
       let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
-  | b[1,1,0,b10,b9,b8,b7,b6] :::
-    b[1,0,b5,b4,b3,b2,b1,b0] ::: s' =>
-      let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10] in
-      if (r <? 0x80)%N then ([], false) else
-      let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
-  | b[1,1,1,0,b15,b14,b13,b12] :::
-    b[1,0,b11,b10,b9,b8,b7,b6] :::
-    b[1,0,b5,b4,b3,b2,b1,b0] ::: s' =>
-      let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10;b11;b12;b13;b14;b15] in
-      if ((r <? 0x800) || ((0xD800 <=? r) && (r <=? 0xDFFF)))%N then ([], false) else
-      let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
-  | b[1,1,1,1,0,b20,b19,b18] :::
-    b[1,0,b17,b16,b15,b14,b13,b12] :::
-    b[1,0,b11,b10,b9,b8,b7,b6] :::
-    b[1,0,b5,b4,b3,b2,b1,b0] ::: s' =>
-      let r := N_of_digits [b0;b1;b2;b3;b4;b5;b6;b7;b8;b9;b10;b11;b12;b13;b14;b15;b16;b17;b18;b19;b20] in
-      if ((r <? 0x10000) || (0x10ffff <? r))%N then ([], false) else
-      let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
-  | _ ::: _ => ([], false)
-  | "" => ([], true)
-  end%string.
+    | 1, 1, 0, _, _ =>
+      match s' with
+      | b[1,0,b13,b12,b11,b10,b9,b8] ::: s' =>
+        let r := N_of_digits [b8;b9;b10;b11;b12;b13;b0;b1;b2;b3;b4] in
+        if (r <? 0x80)%N then ([], false) else
+        let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
+      | _ => ([], false)
+      end
+    | 1, 1, 1, 0, _ =>
+      match s' with
+      | b[1,0,b13,b12,b11,b10,b9,b8] :::
+        b[1,0,b21,b20,b19,b18,b17,b16] ::: s' =>
+          let r := N_of_digits [b16;b17;b18;b19;b20;b21;b8;b9;b10;b11;b12;b13;b0;b1;b2;b3] in
+          if ((r <? 0x800) || ((0xD800 <=? r) && (r <=? 0xDFFF)))%N then ([], false) else
+          let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
+      | _ => ([], false)
+      end
+    | 1, 1, 1, 1, 0 =>
+      match s' with
+      | b[1,0,b13,b12,b11,b10,b9,b8] :::
+        b[1,0,b21,b20,b19,b18,b17,b16] :::
+        b[1,0,b29,b28,b27,b26,b25,b24] ::: s' =>
+          let r := N_of_digits [b24;b25;b26;b27;b28;b29;b16;b17;b18;b19;b20;b21;b8;b9;b10;b11;b12;b13;b0;b1;b2] in
+          if ((r <? 0x10000) || (0x10ffff <? r))%N then ([], false) else
+          let (rs, ok) := decode_utf8 s' in (r :: rs, ok)
+      | _ => ([], false)
+      end
+    | _, _, _, _, _ => ([], false)
+    end
+  | "" => ([], false)
+  end.
 
 Definition rune_encode_utf8 (r : rune) : string :=
   match rune_to_bits r with
@@ -82,4 +93,4 @@ Fixpoint string_is_ascii (s : string) : bool :=
   match s with
   | a ::: s' => (a <? "128")%char && string_is_ascii s'
   | "" => false
-  end%string.
+  end.
